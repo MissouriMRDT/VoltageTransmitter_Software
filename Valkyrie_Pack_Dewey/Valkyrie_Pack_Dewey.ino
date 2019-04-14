8c15f0b791ddd0961c3aa3e3ec4c072176b34b15
Jacob Lipina <jrlwd5@mst.edu>
2019-04-13 21:14:00 -0500

    Added Features

--- Valkyrie_Pack_Dewey/Valkyrie_Pack_Dewey.ino
+++ Valkyrie_Pack_Dewey/Valkyrie_Pack_Dewey.ino
@@ -1,7 +1,7 @@
 // Voltage Transmitter Software //////////////////////////////////////////////////////////////////////////////////////////////////////////
   // Main ino File
 //
-// Created for 2019 Valkyrie by: Eli Verbrugge, Ethan Arneson
+// Created for 2019 Valkyrie by: Eli Verbrugge, Ethan Arneson, Jacob Lipina
 // 
 // ESP8266-12e device plugs into battery pack and relays the measured voltage to the "#batteryvoltage" Slack channel. 
 //
@@ -20,17 +20,21 @@
 #define SEND_MSG_PIN        16 //Pin labeled "D0" is GPIO16
 #define PACK_VOLTS_MAX      33600 // mvolts
 #define PACK_VOLTS_MIN      21600 // mvolts
-#define PACK_VOLTS_ADC_MAX  974
+#define PACK_DISCONNECTED   5 // volts
+#define PACK_VOLTS_ADC_MAX  967
 #define PACK_VOLTS_ADC_MIN  629 
+#define PACK_NAME           "Atlas Pack _Main_" // Choices: "Valkyrie Pack _Dewey_" ; "Valkyrie Pack _TBD_" ; "Atlas Pack _Main_" ; "Atlas Pack _Spare_" ; "Gryphon Pack _Main_" 
+#define PACK_LOGO           ":atlas_logo_short: " // Choices: ":dew: " for Valkyrie Pack Dewey; TBD for Valkyrie Pack 2; ":atlas_logo_short: " for Atlas Pack Main; ":shimble: " for Atlas Pack Spare; ":gryphon_logo3: " for Gryphon Pack Main
 int num_loops               = 0; //num times program has looped
 int adc_reading             = 0;
 float measured_voltage      = 0;
 uint32_t send_msg_timer     = millis();
-const uint32_t SEND_MSG     = 108000000; //30min = 108000000msec 
+const uint32_t SEND_MSG     = 1800000; //30min = 1800000msec 
 uint32_t num_disconnected   = 0;
 
+
 #define STASSID             "MST-PSK-N"
-#define STAPSK             "JoeMiner"
+#define STAPSK              "JoeMiner"
 const char *ssid            = STASSID;
 const char *pass            = STAPSK;
 const char *host            = "hooks.slack.com";
@@ -64,7 +68,9 @@ void setup()
   pinMode(SEND_MSG_PIN, OUTPUT); 
   digitalWrite(SEND_MSG_PIN, LOW); //LED is active low
   
+
   WiFi.mode(WIFI_STA); // We start by connecting to a WiFi network
+  WiFi.setSleepMode(WIFI_NONE_SLEEP);
   WiFi.begin(ssid, pass);
 
   while (WiFi.status() != WL_CONNECTED) {
@@ -84,23 +90,49 @@ void loop()
     Serial.print("ADC: ");
     Serial.println(adc_reading);
 
-    if(adc_reading > PACK_VOLTS_ADC_MAX)
+    if(adc_reading > PACK_VOLTS_ADC_MIN)
     {
-      adc_reading = PACK_VOLTS_ADC_MAX;
+      if(adc_reading > PACK_VOLTS_ADC_MAX)
+      {
+        adc_reading = PACK_VOLTS_ADC_MAX;
+      }
+
+      measured_voltage = map(adc_reading, PACK_VOLTS_ADC_MIN, PACK_VOLTS_ADC_MAX, PACK_VOLTS_MIN, PACK_VOLTS_MAX);
+    }
+
+    if(adc_reading < PACK_VOLTS_ADC_MIN)
+    {
+      measured_voltage = map(adc_reading, 0, PACK_VOLTS_ADC_MIN, 0, PACK_VOLTS_MIN);
     }
     
-    measured_voltage = map(adc_reading, PACK_VOLTS_ADC_MIN, PACK_VOLTS_ADC_MAX, PACK_VOLTS_MIN, PACK_VOLTS_MAX);
     measured_voltage = measured_voltage / 1000;
     measured_voltage = roundf(measured_voltage * 100) / 100; //Rounds voltage to two num after decimal
     //Serial.println(measured_voltage);
 
-    if(measured_voltage < 21.6 && num_loops != 0)
+    if(measured_voltage < PACK_DISCONNECTED && num_loops != 0)
     {
-      message = ":boi_matt2: Valkyrie Pack _Dewey_ *DISCONNECTED*";
+      message = ":boi_matt2: ";
+      message += patch::to_string(PACK_NAME);
+      message += " *DISCONNECTED*";
       fetchInsecure();
       num_disconnected ++;
       send_msg_timer = millis();
-      digitalWrite(SEND_MSG_PIN, HIGH);
+      digitalWrite(SEND_MSG_PIN, HIGH); // Blink blue led to indicate msg sent
+      delay(250);
+      digitalWrite(SEND_MSG_PIN, LOW);
+    }
+    if(measured_voltage > PACK_DISCONNECTED && measured_voltage < 21.6)
+    {
+      Serial.print("Voltage: ");
+      Serial.println(measured_voltage);
+      message = "<!channel> :fire: ";
+      message += patch::to_string(PACK_NAME);
+      message += " DAMAGED:  *";
+      message += patch::to_string(measured_voltage);
+      message += "V*";
+      fetchInsecure();
+      send_msg_timer = millis();
+      digitalWrite(SEND_MSG_PIN, HIGH); // Blink blue led to indicate msg sent
       delay(250);
       digitalWrite(SEND_MSG_PIN, LOW);
     }
@@ -108,12 +140,14 @@ void loop()
     {
       Serial.print("Voltage: ");
       Serial.println(measured_voltage);
-      message = "<!channel> :rotating_light: Valkyrie Pack _Dewey_ CRITICAL:  *";
+      message = "<!channel> :rotating_light: ";
+      message += patch::to_string(PACK_NAME);
+      message += " CRITICAL:  *";
       message += patch::to_string(measured_voltage);
       message += "V*";
       fetchInsecure();
       send_msg_timer = millis();
-      digitalWrite(SEND_MSG_PIN, HIGH);
+      digitalWrite(SEND_MSG_PIN, HIGH); // Blink blue led to indicate msg sent
       delay(250);
       digitalWrite(SEND_MSG_PIN, LOW);
     }
@@ -121,12 +155,14 @@ void loop()
     {
       Serial.print("Voltage: ");
       Serial.println(measured_voltage);
-      message = ":warning: Valkyrie Pack _Dewey_ LOW:  *";
+      message = ":warning: ";
+      message += patch::to_string(PACK_NAME);
+      message += "LOW:  *";
       message += patch::to_string(measured_voltage);
       message += "V*";
       fetchInsecure();
       send_msg_timer = millis();
-      digitalWrite(SEND_MSG_PIN, HIGH);
+      digitalWrite(SEND_MSG_PIN, HIGH); // Blink blue led to indicate msg sent
       delay(250);
       digitalWrite(SEND_MSG_PIN, LOW);
     }
@@ -134,12 +170,14 @@ void loop()
     {
       Serial.print("Voltage: ");
       Serial.println(measured_voltage);
-      message = ":dew: Valkyrie Pack _Dewey_:  *";
+      message = patch::to_string(PACK_LOGO);
+      message += patch::to_string(PACK_NAME);
+      message += ":  *";
       message += patch::to_string(measured_voltage);
       message += "V*";
       fetchInsecure();
       send_msg_timer = millis();
-      digitalWrite(SEND_MSG_PIN, HIGH);
+      digitalWrite(SEND_MSG_PIN, HIGH); // Blink blue led to indicate msg sent
       delay(250);
       digitalWrite(SEND_MSG_PIN, LOW);
     }   
